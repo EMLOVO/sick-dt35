a54b099d41f34dc40957517c87d11d6fa0f14597
Cheng Man Man <141893453+EMLOVO@users.noreply.github.com>
2025-08-18 22:58:18 +0800

    Intialize.ino

--- position_control_test/position_control_test.ino
+++ position_control_test/position_control_test.ino
@@ -3,24 +3,23 @@
 
 #define clamp 13
 #define sensor 32
+
 Rm_set motors;
 int initial_pos;
 
-const int down = 400000;
+int homing_stage = -1;
+int initial_stage = -1;
+int initial_encoder = 0;
 
-int homing_stage_y = -1;
-int initial_stage_y = -1;
-int y_initial_encoder = 0;
-
-void IRAM_ATTR ISR_Y() {
-  if (homing_stage_y > 0) {
-    homing_stage_y = 0;
-    initial_stage_y = 0;
+void IRAM_ATTR ISR() {
+  if (homing_stage > 0) {
+    homing_stage = 0;
+    initial_stage = 0;
   }
 }
 
 void find_home() {
-  homing_stage_y = 1;
+  homing_stage = 1;
   digitalWrite(clamp,HIGH);
 
 }
@@ -41,16 +40,16 @@ void can1_callback(CAN_FRAME *frame)
     int rx_id = frame->id;
     motors.update_motor_status(rx_id - 0x201, micros(), frame->data.uint8[0] << 8 | frame->data.uint8[1], frame->data.uint8[2] << 8 | frame->data.uint8[3]);
 
-  if (homing_stage_y == 1) {
+  if (homing_stage == 1) {
       motors.set_target_rpm(0, 1000);
     } 
 
-  if (initial_stage_y == 0) {
+  if (initial_stage == 0) {
     motors.set_target_rpm(0, 0);
-    y_initial_encoder = motors.gearbox_pos[0];
+    initial_encoder = motors.gearbox_pos[0];
     //Serial.printf("read!");
     // digitalWrite(clamp, HIGH);
-    initial_stage_y = 1;
+    initial_stage = 1;
   }
 
 
@@ -67,13 +66,12 @@ void setup()
     motors.set_pid_rpm(i, 6, 0.01, 0); //set pid for each rm aqW22
     motors.reset_gearbox_pos(i); //reset the encoder position to 0
   }
-  digitalWrite(clamp, LOW);
   CAN1.begin(1000000);   
   CAN1.watchFor();
   CAN1.setGeneralCallback(can1_callback);
 
   Serial.println(motors.gearbox_pos[0]);
-  attachInterrupt(sensor, ISR_Y, FALLING);
+  attachInterrupt(sensor, ISR, FALLING);
 
   find_home();
 }
@@ -103,4 +101,5 @@ void loop()
     // Serial.println(motors.gearbox_pos[0]);
     //Serial.println(digitalRead(sensor));
     send_rm_frame(); // 記得send番個rm frame
+
 }
